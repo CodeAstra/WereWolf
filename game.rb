c433da9578596d07f24f111e364a2886511d06e8
BV Satyaram <bvsatyaram@gmail.com>
2015-10-17 16:31:03 +0530

    Run voting before killing a player in the day mode

--- game.rb
+++ game.rb
@@ -40,11 +40,40 @@ private
   end
 
   def night_mode
-    @villagers.select(&:alive?).sample.kill!
+    victim = @villagers.select(&:alive?).sample
+    victim.kill!
+    puts "#{victim} got killed in night mode"
   end
 
   def day_mode
-    (@villagers + @wolves).sample.kill!
+    victim = run_voting
+    victim.kill!
+    puts "#{victim} got killed in day mode"
+  end
+
+  def run_voting
+    votes = Hash.new(0)
+
+    wolves_alive.each do
+      player = villagers_alive.sample
+      votes[player] += 1
+    end
+    villagers_alive.each do |villager|
+      player = (villagers_alive + wolves_alive - [villager]).sample
+      votes[player] += 1
+    end
+
+    max_votes = votes.values.max
+    winners = []
+    votes.keys.each do |player|
+      winners.push(player) if votes[player] == max_votes
+    end
+
+    if winners.count == 1
+      return winners.first
+    else
+      return run_voting
+    end
   end
 
   def villagers_alive
@@ -64,4 +93,4 @@ private
   end
 end
 
-Game.new(3, 6).simulate
+Game.new(3, 15).simulate
