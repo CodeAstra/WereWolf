aa3c049d8c5bfc7baa0f32a636aa67f7ad4749a8
BV Satyaram <bvsatyaram@gmail.com>
2015-10-18 16:13:36 +0530

    Modify params to account to stupidity of bot-villagers

--- game_suggestor.rb
+++ game_suggestor.rb
@@ -8,23 +8,25 @@ class GameSuggestor
   end
 
   def run
+    # Preferred Wolves Win Percentage
+    pref_wolves_prob = 70
     wolves_count = 1
-    absolute_diff = 50
+    absolute_diff = 1000
     best_wins = {}
 
     while true
       wins = GameSimulator.new(wolves_count, @players_count - wolves_count, @no_of_runs).run
-      percent_wolves_win = (100.0*wins[Game::WOLF]/(wins[Game::WOLF] + wins[Game::VILLAGER]))
-      new_diff = (50 - percent_wolves_win).abs
+      percent_wolves_win = (100.0*wins[Game::WOLF]/@no_of_runs)
+      new_diff = (pref_wolves_prob - percent_wolves_win).abs
       if new_diff < absolute_diff
         absolute_diff = new_diff
         best_wins = wins
         wolves_count += 1
       else
         puts best_case_description(wolves_count - 1)
-        puts "Probability of Wolves Winning:    #{100.0*best_wins[Game::WOLF]/@no_of_runs}%"
-        puts "Probability of Villagers Winning: #{100.0*best_wins[Game::VILLAGER]/@no_of_runs}%"
-        puts "Probability of Draw:              #{100.0*best_wins[Game::DRAW]/@no_of_runs}%"
+        puts "Probability of Wolves Winning:    #{100.0*(best_wins[Game::WOLF] || 0)/@no_of_runs}%"
+        puts "Probability of Villagers Winning: #{100.0*(best_wins[Game::VILLAGER] || 0)/@no_of_runs}%"
+        puts "Probability of Draw:              #{100.0*(best_wins[Game::DRAW] || 0)/@no_of_runs}%"
         break
       end
     end
