0fea60470011a33c3aef628c073441ec8730a93e
BV Satyaram <bvsatyaram@gmail.com>
2015-10-19 15:51:09 +0530

    Changing forgot change

--- lib/werewolf/game_simulator.rb
+++ lib/werewolf/game_simulator.rb
@@ -10,7 +10,7 @@ class GameSimulator
   def simulate
     wins = Hash.new(0)
     @no_of_runs.times do
-      wins[Game.new(@no_of_wolves, @no_of_villagers).simulate] += 1
+      wins[Game.new(@no_of_wolves, @no_of_villagers).run] += 1
     end
 
     return wins
